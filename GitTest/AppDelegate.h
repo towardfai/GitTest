fb772ac1942d6ed095781fc7d84fdbafc4a388ec
Towardfai <towardfai@towardfaidemacbook-air.local>
2016-02-18 16:27:06 +0800

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Towardfai on 16/2/18.
+//  Copyright © 2016年 ledao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest
+//
+//  Created by Towardfai on 16/2/18.
+//  Copyright © 2016年 ledao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
